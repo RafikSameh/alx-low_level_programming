096b4be1ceb935869e458157fa19e6b6c88f12e9
RafikSameh <rafiksameh123@gmail.com>
2023-03-16 18:13:41 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main is the main function
+ * n
+ * return = 0 (success)
+ */
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
