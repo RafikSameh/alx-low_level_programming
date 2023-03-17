fe7a683c90e90cba115c9c60d38ebcfc236e3ba1
RafikSameh <rafiksameh123@gmail.com>
2023-03-17 16:27:28 -0700

    0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - is a function to check the output is negative or positive
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	if(n<0)
+	{
+		printf("%d is negative",n);
+	}
+	else if(n>0)
+	{
+		printf("%d is positive",n);
+	}
+	else
+	{
+		printf("is zero");
+	}
+	return (0);
+}
