a8edfeacfdbb56efbceafcacdbe267d0439eb2c8
RafikSameh <rafiksameh123@gmail.com>
2023-03-17 17:24:37 -0700

    manga

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - it is a function
+ * followed by a new line
+ * Return : 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
