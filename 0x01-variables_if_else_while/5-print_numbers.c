fcbcc2f244aa055b7d58abb7fafd0bc3e847aa09
RafikSameh <rafiksameh123@gmail.com>
2023-03-17 17:30:51 -0700

    4

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - is a function
+ * flollowed by a new line
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0 ; n <= 10 ; n++)
+	{
+		printf("%d\n", n);
+	}
+	return (0);
+}
