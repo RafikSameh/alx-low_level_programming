f97dd0dc767bc33831d03ae4857e93fc2c75a01c
RafikSameh <rafiksameh123@gmail.com>
2023-04-11 16:40:29 -0700

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,14 +12,14 @@ char *create_array(unsigned int size, char c)
 	char *str;
 	unsigned int i;
 
-	str=malloc(size * sizeof(char));
-    	if(size == 0 || str == NULL)
-    	{
-        	return (NULL);
+	str = malloc(size * sizeof(char));
+	if(size == 0 || str == NULL)
+	{
+		return (NULL);
     	}
-    	for(i = 0; i < size; i++)
-    	{
-        	str[i] = c;
-    	}
-    	return (str);
+	for(i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+	return (str);
 }
