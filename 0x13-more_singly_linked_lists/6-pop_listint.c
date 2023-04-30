464744ef96fdbeca57f86a79d34b9c318b2d9939
RafikSameh <rafiksameh123@gmail.com>
2023-04-29 17:14:41 -0700

    6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,12 +12,10 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int num;
 
-	if (head == NULL)
-		return (0);
-	if (*head == NULL)
+	if (!head || !*head)
 		return (0);
 
-	if (*head)
+	else
 	{
 		temp = (*head)->next;
 		num = (*head)->n;
