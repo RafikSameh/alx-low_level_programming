b903f9cc231dd4f182b4642904fc3e4857b87cf3
RafikSameh <rafiksameh123@gmail.com>
2023-04-29 17:02:17 -0700

    5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - is a function that free list
+ * @head: is pointer to pointer
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = *head;
+
+	if (!head)
+		return;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
+}
