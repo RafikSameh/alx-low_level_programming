80243b99cecffa68ae65c0fc51c294731b475527
RafikSameh <rafiksameh123@gmail.com>
2023-05-04 17:19:02 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,7 +16,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
 
 		dec = 2 * dec + (b[i] - '0');
